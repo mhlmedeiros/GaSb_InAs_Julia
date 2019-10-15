a906eecc3460c00e0c32f5367e7ffcd6f1b2e26c
Marcos <mhlmedeiros@gmail.com>
2019-10-14 22:19:54 -0300

    Define named tuples for parameters

--- new_functions.jl
+++ new_functions.jl
@@ -6,84 +6,85 @@ using Plots
 #                         Sistema com 97.0 Å (Angstrom):
 #                       Retirado de Ham3x3-097-103-110.nb
 #*******************************************************************************
-
-EnCB_97 = 0.0320879;
-EnVB_97 = 0.0314029;
-GammaCB_97 = 36.917;
-GammaVB_97 = -22.4782;
-P_97 = -0.108812;
-Delta_97 = -0.00019967;
-eta2_97 = -0.280174;
-eta3_97 = -1.16087;
-
+params97 = (
+    EnC = 0.0320879,
+    EnV = 0.0314029,
+    gammaC = 36.917,
+    gammaV = -22.4782,
+    P = -0.108812,
+    delta = -0.00019967,
+    eta2 = -0.280174,
+    eta3 = -1.16087
+);
 
 #*******************************************************************************
 #                         Sistema com 103.0 Å (Angstrom):
 #                       Retirado de Ham3x3-097-103-110.nb
 #*******************************************************************************
 
-
-EnCB_103 = 0.0313913;
-EnVB_103 = 0.0314028;
-GammaCB_103 = 39.9073;
-GammaVB_103 = -21.6738;
-P_103 = 0.0972126;
-Delta_103 = -0.000201637;
-eta2_103 = -0.295556;
-eta3_103 = -1.17352;
-
+params103 = (
+    EnC = 0.0313913,
+    EnV = 0.0314028,
+    gammaC = 39.9073,
+    gammaV = -21.6738,
+    P = 0.0972126,
+    delta = -0.000201637,
+    eta2 = -0.295556,
+    eta3 = -1.17352
+);
 
 #*******************************************************************************
 #                         Sistema com 110.0 Å (Angstrom):
 #                       Retirado de Ham3x3-097-103-110.nb
 #*******************************************************************************
 
-EnCB_110 = 0.030775;
-EnVB_110 = 0.0314028;
-GammaCB_110 = 42.9456;
-GammaVB_110 = -15.4158;
-P_110 = 0.0865811;
-Delta_110 = -0.000203489;
-eta2_110 = 0.309555;
-eta3_110 = 1.18696;
-
+params110 = (
+    EnC = 0.030775,
+    EnV = 0.0314028,
+    gammaC = 42.9456,
+    gammaV = -15.4158,
+    P = 0.0865811,
+    delta = -0.000203489,
+    eta2 = 0.309555,
+    eta3 = 1.18696
+);
 
 #*******************************************************************************
 #                         Hamiltonian Operators
 #*******************************************************************************
 
 
-function Hamil3x3(kx, ky, enCBR, enVBR, γCR, γVR, η2R, η3R, PXR, Δ)
-    H_11 = enCBR + (kx^2 + ky^2) * γCR;
-    H_12 = (kx^2 - ky^2) * η2R + 1im * kx * ky * η3R;
-    H_13 = - 1im * (kx + 1im * ky) * PXR;
+function Hamil3x3(kx, ky, p)
+    H_11 = p.EnC + (kx^2 + ky^2) * p.gammaC;
+    H_12 = (kx^2 - ky^2) * p.eta2 + 1im * kx * ky * p.eta3;
+    H_13 = - 1im * (kx + 1im * ky) * p.P;
 
-    H_21 = (kx^2 - ky^2) * η2R - 1im * kx * ky * η3R;
-    H_22 = enVBR + (kx^2 + ky^2) * γVR;
-    H_23 = -1im * Δ ;
+    H_21 = (kx^2 - ky^2) * p.eta2 - 1im * kx * ky * p.eta3;
+    H_22 = p.EnV + (kx^2 + ky^2) * p.gammaV;
+    H_23 = -1im * p.delta ;
 
-    H_31 = + 1im * (kx - 1im * ky) * PXR;;
-    H_32 = 1im * Δ ;
+    H_31 = + 1im * (kx - 1im * ky) * p.P;;
+    H_32 = 1im * p.delta ;
     H_33 = H_22;
     return [H_11 H_12 H_13; H_21 H_22 H_23; H_31 H_32 H_33]
 end
 
-function Hamil3Nyx3Ny(kx, enCBR, enVBR, γCR, γVR, η2R, η3R, PXR, Δ ; dim = 3)
+function Hamil3Nyx3Ny(kx, p; dim = 3)
     k_y_first = (-1im/(2*dy)) * Tridiagonal(-ones(Ny-1), zeros(Ny), ones(Ny-1))
     k_y_second = (-1/(dy^2)) * SymTridiagonal(-2*ones(Ny), ones(Ny-1))
 
-    H_11 = I*enCBR + (I*kx^2 + k_y_second)*γCR;
-    H_12 = (I*kx^2 - k_y_second)*η2R + 1im*kx*k_y_first*η3R;
-    H_13 = -1im*(kx*I + 1im*k_y_first)*PXR;
+    H_11 = I*p.EnC + (I*kx^2 + k_y_second)*p.gammaC;
+    H_12 = (I*kx^2 - k_y_second)*p.eta2 + 1im*kx*k_y_first*p.eta3;
+    H_13 = -1im*(kx*I + 1im*k_y_first)*p.P;
 
 
     H_21 = transpose(conj(H_12));
-    H_22 = I*enVBR + (I*kx^2 + k_y_second)*γVR;
-    H_23 = -1im*Δ*I;
+    H_22 = I*p.EnV + (I*kx^2 + k_y_second)*p.gammaV;
+    H_23 = -1im*p.delta*I;
 
 
     H_31 = transpose(conj(H_13));
-    H_32 = 1im*Δ*I;
+    H_32 = 1im*p.delta*I;
     H_33 = H_22;
 
     if dim == 2
@@ -93,7 +94,7 @@ function Hamil3Nyx3Ny(kx, enCBR, enVBR, γCR, γVR, η2R, η3R, PXR, Δ ; dim = 
     end
 end
 
-function spectrum_DS(k_vec, enCBR, enVBR, γCR, γVR, η2R, η3R, PXR, Δ; spinor_dim = 3)
+function spectrum_DS(k_vec, p; spinor_dim = 3)
 
     #********************************************************************#
     #           Essa função gera uma matriz em que                       #
@@ -115,8 +116,7 @@ function spectrum_DS(k_vec, enCBR, enVBR, γCR, γVR, η2R, η3R, PXR, Δ; spino
     cont = 0;
     for k in k_vec
         cont += 1;
-        E_vals[:, cont] = eigvals( Hamil3Nyx3Ny(k, enCBR, enVBR, γCR, γVR,
-                            η2R, η3R, PXR, Δ, dim = spinor_dim) );
+        E_vals[:, cont] = eigvals( Hamil3Nyx3Ny(k, p, dim = spinor_dim) );
     end
     return E_vals
 end
@@ -150,7 +150,7 @@ function print_bandas(kx_span, M_vals)
             ylims = (ymin, ymax),
             color = "black",
             legend=false)
-    display(p)
+    return p
 end
 
 
@@ -171,19 +171,16 @@ vec_k_limited = range(-1,stop= 1, length=Nkx) * pi/aSystem * porcent;
 
 chosen_dim = 3;
 
-E_097 = spectrum_DS(vec_k_limited,
-                EnCB_97, EnVB_97,
-                GammaCB_97, GammaVB_97,
-                eta2_97, eta3_97,
-                P_97, Delta_97,
-                spinor_dim = chosen_dim);
+E_097 = spectrum_DS(vec_k_limited, params97, spinor_dim = chosen_dim);
 
 # y_min,y_max = 410,470;
 # x_min,x_max = -0.1,0.1;
 #
 # figure(1)
 # plot(vec_k_limited./(a₀*10^(-1)), Ry * 1000 * E_097')
-print_bandas(vec_k_limited,  E_097)
+fig = print_bandas(vec_k_limited,  E_097)
+# display(display(fig))
+savefig(fig,"bands_97_old_params.png")
 # ylim(y_min,y_max)
 # xlim(x_min,x_max
 # length(vec_k_limited)
